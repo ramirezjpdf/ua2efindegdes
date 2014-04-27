d68736158f640b9a889f4b667b40de43da319b70
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-04-27 16:56:09 -0300

    UA2EFindEgesApp added

---
+++ src/com/example/ua2efindedges/UA2EFindEdgesApp.java
@@ -0,0 +1,18 @@
+package com.example.ua2efindedges;
+
+import android.app.Application;
+import android.graphics.Bitmap;
+
+public class UA2EFindEdgesApp extends Application{
+	private Bitmap b;
+
+	public Bitmap getB() {
+		return b;
+	}
+
+	public void setB(Bitmap b) {
+		this.b = b;
+	}
+	
+
+}
