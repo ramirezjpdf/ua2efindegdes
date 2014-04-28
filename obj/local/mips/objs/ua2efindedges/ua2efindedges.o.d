d0cfc0f6f28ba97c281143c6d33d30fa44fbd3c2
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-04-28 07:38:20 -0300

    supporting all platforms

---
+++ obj/local/mips/objs/ua2efindedges/ua2efindedges.o.d
@@ -0,0 +1,31 @@
+obj/local/mips/objs/ua2efindedges/ua2efindedges.o: jni/ua2efindedges.c \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/jni.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/api-level.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/log.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/bitmap.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/stdint.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/_types.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/machine/_types.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/_wchar_limits.h
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/jni.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/api-level.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/log.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/android/bitmap.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/stdint.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/_types.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/machine/_types.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-mips/usr/include/sys/_wchar_limits.h:
---
+++ obj/local/x86/objs/ua2efindedges/ua2efindedges.o.d
@@ -0,0 +1,31 @@
+obj/local/x86/objs/ua2efindedges/ua2efindedges.o: jni/ua2efindedges.c \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/jni.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/log.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/bitmap.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/jni.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/bitmap.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/cygdrive/c/Java/android/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/_wchar_limits.h:
